a778eaf152f3c9cd9433d99f8405deae14efa9cd
unknown <lalorodriguezz>
2025-02-15 15:02:25 -0600

    configurando cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,20 +1,16 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   projectId: "64y54w",
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "vite",
-    },
-    specPattern: 'src/test/*/.{cy,test}.{js,jsx,ts,tsx}',
-    supportFile: false, 
-  },
   e2e: {
     baseUrl: 'http://localhost:5173',
+    specPattern: '**/*.spec.ts',
     supportFile: false,
+
+    // Record screenshots and videos
     video: true,
     videoCompression: 32,
     screenshots: true,
   },
-});
+  testFiles: '**/*.spec.ts',
+})

bfbb7661457976764e141663ef3ea9952fb98454
unknown <lalorodriguezz>
2025-02-15 14:13:33 -0600

    configurando cypress.config.ts

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,19 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: "64y54w",
+  projectId: "64y54w", // ID del proyecto
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: "http://localhost:5173", // URL base para pruebas E2E
+    supportFile: false, // Desactiva el archivo de soporte
+    video: true, // Habilita grabación de videos
+    videoCompression: 32, // Compresión de videos para reducir tamaño
+    screenshots: true, // Habilita capturas de pantalla
   },
-})
+  component: {
+    devServer: {
+      framework: "react", // Framework utilizado
+      bundler: "vite", // Bundler utilizado
+    },
+    specPattern: "src/**/*.cy.tsx", // Patrón para archivos de pruebas de componentes
+  },
+});
